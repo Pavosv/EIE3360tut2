14b483216889a785f1257c611a3ec4668140c511
Pavosv <pavelosvaldoh@gmail.com>
2024-02-02 18:39:17 +0800

    Exercise 2

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public Text GameOverText;
+
+    //Animator anim;
+
+
+    void Awake()
+    {
+        GameOverText.enabled = false;
+        //anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            GameOverText.enabled = true;
+            //anim.SetTrigger("GameOver");
+        }
+    }
+}
